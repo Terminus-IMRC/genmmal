76eed81424071e127bd33e58494f54d16a0a71af
Sugizaki Yukimasa <ysugi@idein.jp>
2018-06-26 19:12:40 +0900

    Forget to add header files for C files

---
+++ genmmal.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Sugizaki Yukimasa (ysugi@idein.jp)
+ * All rights reserved.
+ *
+ * This software is licensed under a Modified (3-Clause) BSD License.
+ * You should have received a copy of this license along with this
+ * software. If not, contact the copyright holder above.
+ */
+
+#ifndef GENMMAL_H_
+#define GENMMAL_H_
+
+    int genmmal_init(void);
+    int genmmal_finl(void);
+
+#endif /* GENMMAL_H_ */
